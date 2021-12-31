6da27a3f93c344f2bd7b6f6100d7f87a0c6f1007
Daniel Upstone <zealvurte@users.noreply.github.com>
2021-12-31 02:39:33 +0000

    Updated with latest data

--- SimData-Spells_tsv-to-lua.lua
+++ SimData-Spells_tsv-to-lua.lua
@@ -1033,26 +1033,26 @@ local vpData = {
 		[2]={type="aura", plusDamageTakenATK=20, target=1, duration=3},	-- Tainted Bite: Mod damage taken of furthest enemy by (0.2*attack) for 3 rounds [VERIFIED]
 	},
 	[349]={type="nuke", damageATK=10, target="all-enemies"},	-- Anima Swell: Damage all enemies for (0.1*attack) [VERIFIED]
-	[350]={type="nuke", damageATK=25, target="cleave"},	-- Attack Wave: Damage closest enemies for (0.25*attack) [UNVERFIED]
-	[351]={type="nuke", damageATK=75, target=1, firstTurn=4},	-- Attack Pulse: Damage furthest enemy for (0.75*attack) [UNVERFIED]
-	[352]={type="aura", modDamageTaken=30, target=4},	-- Active Shielding: Mod damage taken of self by 30% for 0 rounds [UNVERFIED]
-	[353]={type="aura", modDamageDealt=20, target=1},	-- Disruptive Field: Mod damage done of furthest enemy by 20% for 0 rounds [UNVERFIED]
-	[354]={type="nuke", damageATK=400, target="enemy-front", firstTurn=5},	-- Energy Blast: Damage frontmost row of enemies for (4*attack) [UNVERFIED]
-	[355]={type="passive", modDamageDealt=-25, target=1},	-- Mitigation Aura: Mod damage done of furthest enemy by -25% indefinitely [UNVERFIED]
+	[350]={type="nuke", damageATK=25, target="cleave"},	-- Attack Wave: Damage closest enemies for (0.25*attack) [VERIFIED]
+	[351]={type="nuke", damageATK=75, target=1, firstTurn=4},	-- Attack Pulse: Damage furthest enemy for (0.75*attack) [VERIFIED]
+	[352]={type="aura", modDamageTaken=30, target=4},	-- Active Shielding: Mod damage taken of self by 30% for 0 rounds [VERIFIED] #Bug/#Workaround: ignored incorrect Spell.Duration
+	[353]={type="aura", modDamageDealt=20, target=1},	-- Disruptive Field: Mod damage done of furthest enemy by 20% for 0 rounds [VERIFIED] #Bug/#Workaround: ignored incorrect Spell.Duration
+	[354]={type="nuke", damageATK=400, target="enemy-front", firstTurn=5},	-- Energy Blast: Damage frontmost row of enemies for (4*attack) [VERIFIED]
+	[355]={type="passive", modDamageDealt=-25, target=1},	-- Mitigation Aura: Mod damage done of furthest enemy by -25% indefinitely [VERIFIED]
 	[356]={type="nuke", damageATK=100, target=1},	-- Bone Ambush: Damage furthest enemy for (1*attack) [UNVERFIED] #Bug/#Workaround: ignored incorrect Effect.Target
 	[357]={type="passive", modDamageDealt=-50, target=0},	-- Mitigation Aura: Mod damage done of closest enemy by -50% indefinitely [VERIFIED]
 	[358]={type="nuke", damageATK=400, target="enemy-front", firstTurn=5},	-- Deconstructive Slam: Damage frontmost row of enemies for (4*attack) [VERIFIED]
-	[359]={type="aura", damageATK=50, target=1, duration=4, period=3, noFirstTick=true},	-- Pain Projection: Damage (tick) furthest enemy for (0.5*attack) each subsequent 3rd round for 3 rounds [UNVERFIED]
-	[360]={type="nuke", damageATK=50, target="enemy-front"},	-- Anima Draw: Damage frontmost row of enemies for (0.5*attack) [UNVERFIED]
+	[359]={type="aura", damageATK=50, target=1, duration=4, period=3, noFirstTick=true},	-- Pain Projection: Damage (tick) furthest enemy for (0.5*attack) each subsequent 3rd round for 3 rounds [VERIFIED]
+	[360]={type="nuke", damageATK=50, target="enemy-front"},	-- Anima Draw: Damage frontmost row of enemies for (0.5*attack) [VERIFIED]
 	[361]={type="nuke", damageATK=75, target="enemy-front"},	-- Geostorm: Damage frontmost row of enemies for (0.75*attack) [UNVERFIED]
 	[362]={type="nuke", damageATK=120, target=1},	-- Anima Stinger: Damage furthest enemy for (1.2*attack) [UNVERFIED]
-	[363]={type="aura", modDamageDealt=10, target="friend-front-hard", duration=2},	-- Pack Instincts: Mod damage done of frontmost row of allies by 10% for 2 rounds [UNVERFIED]
-	[364]={type="taunt", target="all-enemies", duration=2},	-- Intimidating Presence: Taunt all enemies for 2 rounds [UNVERFIED]
-	[365]={type="aura", modDamageTaken=50, target=0, duration=1},	-- Mawsworn Strength: Mod damage taken of closest enemy by 50% for 1 rounds [UNVERFIED]
-	[366]={type="nuke", damageATK=50, target="enemy-front"},	-- Domination Lash: Damage frontmost row of enemies for (0.5*attack) [UNVERFIED]
+	[363]={type="aura", modDamageDealt=10, target="friend-front-hard", duration=2},	-- Pack Instincts: Mod damage done of frontmost row of allies by 10% for 2 rounds [VERIFIED]
+	[364]={type="taunt", target="all-enemies", duration=2},	-- Intimidating Presence: Taunt all enemies for 2 rounds [VERIFIED]
+	[365]={type="aura", modDamageTaken=50, target=0, duration=1},	-- Mawsworn Strength: Mod damage taken of closest enemy by 50% for 1 rounds [VERIFIED]
+	[366]={type="nuke", damageATK=50, target="enemy-front"},	-- Domination Lash: Damage frontmost row of enemies for (0.5*attack) [VERIFIED]
 	[367]={type="nuke", damageATK=75, target="cone"},	-- Domination Thrust: Damage closest cone of enemies for (0.75*attack) [VERIFIED]
 	[368]={type="nuke", damageATK=60, target=1},	-- Domination Bombardment: Damage furthest enemy for (0.6*attack) [VERIFIED]
-	[369]={type="aura", damagePerc=100, target="all-enemies", duration=3, period=2, nore=true},	-- Power of Domination: Damage (tick) all enemies for 100% immediately and each subsequent 2nd round for 2 rounds [VERIFIED] #Bug/#Workaround: ignored incorrect Effect.Type or Effect.Flags
+	[369]={type="aura", damagePerc=100, target="all-enemies", duration=3, period=2, nore=true},	-- Power of Domination: Damage (tick) all enemies for 100% immediately and each subsequent 2nd round for 2 rounds [VERIFIED] #Bug/#Workaround: ignored incorrect Effect.Flags
 	[370]={type="aura", modDamageDealt=-50, target="all-enemies", duration=2},	-- Dominating Presence: Mod damage done of all enemies by -50% for 2 rounds [VERIFIED]
 	[371]={type="aura", modDamageTaken=-25, target="all-other-allies", duration=2},	-- Acceleration Field: Mod damage taken of all-other allies by -25% for 2 rounds [VERIFIED]
 	[372]={type="nuke", damageATK=80, target="enemy-front"},	-- Mace Smash: Damage frontmost row of enemies for (0.8*attack) [UNVERFIED]
@@ -1433,7 +1433,8 @@ local function compareToVP(b,useOriginalVP)
 end
 
 -- [ Data ]
-local data = [[1	DNT JasonTest Envirospell	2	1	0	0	0	1	350	24	1	0	Damage all encounters for (1*attack) 	#N/A	Unused
+local data = [[
+1	DNT JasonTest Envirospell	2	1	0	0	0	1	350	24	1	0	Damage all encounters for (1*attack) 	#N/A	Unused
 2	DNT JasonTest Ability Spell	4	2	1	0	0	19	0.2	22	1	2	Mod damage done of all-other allies by (0.2*attack) for 2 rounds	#N/A	"Unused
 Ignored: ineffective Effect.Period"
 2	DNT JasonTest Ability Spell	4	2	1	0	1	4	1	1	0	0	Heal self for 100% 	#N/A	Unused
@@ -1888,17 +1889,17 @@ Ignored: ineffective Effect.Period"
 348	Tainted Bite	5	3	0	100000	0	3	1.2	5	1	0	Damage furthest enemy for (1.2*attack) 	TRUE	
 348	Tainted Bite	5	3	0	100000	1	20	0.2	5	1	0	Mod damage taken of furthest enemy by (0.2*attack) for 3 rounds	TRUE	
 349	Anima Swell	4	0	0	1000000	0	3	0.1	7	1	0	Damage all enemies for (0.1*attack) 	TRUE	
-350	Attack Wave	4	0	0	1000000	0	3	0.25	9	1	0	Damage closest enemies for (0.25*attack) 		
-351	Attack Pulse	4	0	1	1000000	0	3	0.75	5	1	0	Damage furthest enemy for (0.75*attack) 		
-352	Active Shielding	4	0	0	1000000	0	14	0.3	1	1	2	Mod damage taken of self by 30% for 0 rounds		
-353	Disruptive Field	4	0	0	1000000	0	12	0.2	5	1	2	Mod damage done of furthest enemy by 20% for 0 rounds		
-354	Energy Blast	5	0	1	1000000	0	3	4	15	1	0	Damage frontmost row of enemies for (4*attack) 		
-355	Mitigation Aura	0	0	0	1000000	0	12	-0.25	5	0	0	Mod damage done of furthest enemy by -25% indefinitely		
+350	Attack Wave	4	0	0	1000000	0	3	0.25	9	1	0	Damage closest enemies for (0.25*attack) 	TRUE	
+351	Attack Pulse	4	0	1	1000000	0	3	0.75	5	1	0	Damage furthest enemy for (0.75*attack) 	TRUE	
+352	Active Shielding	4	0	0	1000000	0	14	0.3	1	1	2	Mod damage taken of self by 30% for 0 rounds	TRUE	Ignored: incorrect Spell.Duration
+353	Disruptive Field	4	0	0	1000000	0	12	0.2	5	1	2	Mod damage done of furthest enemy by 20% for 0 rounds	TRUE	Ignored: incorrect Spell.Duration
+354	Energy Blast	5	0	1	1000000	0	3	4	15	1	0	Damage frontmost row of enemies for (4*attack) 	TRUE	
+355	Mitigation Aura	0	0	0	1000000	0	12	-0.25	5	0	0	Mod damage done of furthest enemy by -25% indefinitely	TRUE	
 356	Bone Ambush	2	0	0	1	0	1	2	5	1	3	Damage furthest enemy for (1*attack) 		Ignored: incorrect Effect.Target
 357	Mitigation Aura	0	0	0	1000000	0	12	-0.5	3	0	0	Mod damage done of closest enemy by -50% indefinitely	TRUE	
 358	Deconstructive Slam	5	0	1	1	0	3	4	15	1	0	Damage frontmost row of enemies for (4*attack) 	TRUE	
-359	Pain Projection	5	3	0	100000	0	7	0.5	5	1	3	Damage (tick) furthest enemy for (0.5*attack) each subsequent 3rd round for 3 rounds		
-360	Anima Draw	3	0	0	1000000	0	3	0.5	15	1	0	Damage frontmost row of enemies for (0.5*attack) 		
+359	Pain Projection	5	3	0	100000	0	7	0.5	5	1	3	Damage (tick) furthest enemy for (0.5*attack) each subsequent 3rd round for 3 rounds	TRUE	
+360	Anima Draw	3	0	0	1000000	0	3	0.5	15	1	0	Damage frontmost row of enemies for (0.5*attack) 	TRUE	
 361	Geostorm	4	0	0	1	0	3	0.75	15	1	0	Damage frontmost row of enemies for (0.75*attack) 		
 362	Anima Stinger	4	0	0	1000000	0	3	1.2	5	1	0	Damage furthest enemy for (1.2*attack) 		
 363	Pack Instincts	5	2	0	1000	0	12	0.1	14	0	0	Mod damage done of frontmost row of allies by 10% for 2 rounds	TRUE	
@@ -1907,7 +1908,7 @@ Ignored: ineffective Effect.Period"
 366	Domination Lash	4	0	0	100000	0	3	0.5	15	1	0	Damage frontmost row of enemies for (0.5*attack) 	TRUE	
 367	Domination Thrust	4	0	0	100000	0	3	0.75	11	1	0	Damage closest cone of enemies for (0.75*attack) 	TRUE	
 368	Domination Bombardment	3	0	0	100000	0	3	0.6	5	1	0	Damage furthest enemy for (0.6*attack) 	TRUE	
-369	Power of Domination	4	2	0	1	0	7	1	7	10	2	Damage (tick) all enemies for 100% immediately and each subsequent 2nd round for 2 rounds	TRUE	Ignored: incorrect Effect.Type or Effect.Flags
+369	Power of Domination	4	2	0	1	0	7	1	7	10	2	Damage (tick) all enemies for 100% immediately and each subsequent 2nd round for 2 rounds	TRUE	Ignored: incorrect Effect.Flags
 370	Dominating Presence	5	2	0	1	0	12	-0.5	7	0	2	Mod damage done of all enemies by -50% for 2 rounds	TRUE	
 371	Acceleration Field	5	2	0	1000	0	14	-0.25	22	0	0	Mod damage taken of all-other allies by -25% for 2 rounds	TRUE	
 372	Mace Smash	4	0	0	1	0	3	0.8	15	1	0	Damage frontmost row of enemies for (0.8*attack) 		
